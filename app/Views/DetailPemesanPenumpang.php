218c608edb116c502d98082d705e985673347ddf
Alifan12 <alif129310@gmail.com>
2022-01-11 12:27:48 +0700

    alifa commit

--- app/Views/DetailPemesanPenumpang.php
+++ app/Views/DetailPemesanPenumpang.php
@@ -5,23 +5,118 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="DetailPemesanPenumpang.css">
   <title>SWINGS.com</title>
+  <style>
+  .navbar-menu {
+    width: auto;
+    height: 200px;
+    left: 0px;
+    top: 0px;
+
+    background: #B5CEFF;
+  }
+
+  .navbar {
+    width: auto;
+    height: 70px;
+    background: rgb(9, 30, 68);
+    border-radius: 5px;
+  }
+
+  .logo {
+    width: 50px;
+    margin: 7px 190px;
+    height: 50px;
+    background-size: 40px 40px;
+    border: solid 2px #666;
+    border-radius: 50px;
+    background-color: #fff;
+  }
+
+  .navbar-left {
+    float: left;
+    width: 290px;
+    padding-top: 10pt;
+    padding-left: 10pt;
+    color: #FFFFFF;
+    font-weight: bold;
+    font-size: larger;
+    margin: 10px 10px;
+    letter-spacing: 2px;
+  }
+
+  .rules-booking {
+    text-align: center;
+  }
+
+  .rules-booking ul {
+    display: inline-block;
+    margin-top: 50px;
+    padding: 0;
+    zoom: 1;
+  }
+
+  .rules-booking li {
+    float: left;
+    padding: 2px 5px;
+    list-style: none;
+  }
+
+  .border {
+    align-content: center;
+    margin: 0 auto;
+    width: 500px;
+    border-width: 1px;
+    align-items: center;
+    border-bottom-style: solid;
+    border-bottom-color: dimgray;
+  }
+
+  .detail-penerbangan {
+    margin-top: 20px;
+    width: 50%;
+    height: 100px;
+    background-color: #FFFFFF;
+  }
+  </style>
 </head>
 
 <body>
-  <nav class="navbar">
-    <div class="navbar-left">
-      <div class="logo"></div>
+  <nav class="navbar-menu">
+    <div class="navbar">
+      <div class="navbar-left">
+        SWINGS.com
+      </div>
+
+      <div class="navbar-right">
+        <div class="logo">
+        </div>
+      </div>
+    </div>
+    <div class="rules-booking">
+      <ul>
+        <li>1. Pesan</li>
+        <li>></li>
+        <li>2. Bayar</li>
+        <li>></li>
+        <li>3. Selesai</li>
+      </ul>
+      <div class="border"></div>
     </div>
 
-    <div class="navbar-right">
-      <?php $i = 1; ?>
-      <?php foreach ($account as $a) : ?>
-      <?php $username; ?>
-      <?php endforeach; ?>
-
-    </div>
   </nav>
+  <div class="detail">
+    <div class="detail-pemesan">
+
+    </div>
+    <div class="detail-pemenang">
+
+    </div>
+    <div class="detail-penerbangan">
+
+    </div>
+  </div>
 </body>
 
 </html>
