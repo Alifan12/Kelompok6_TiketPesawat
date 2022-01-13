1d0f0f71660f824651e779094fa347fd6988e0a9
Alifan12 <alif129310@gmail.com>
2022-01-13 19:52:13 +0700

    alifa commit

---
+++ app/Controllers/DetailPemesanController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class DetailPemesanController extends BaseController
+{
+  public function tampilDetail()
+  {
+    return view('/DetailPemesanPenumpang.php');
+  }
+}
