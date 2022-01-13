841aa26a92fc563572980fd465dd85f3387d940a
L4lu4rdit44rip <ardita.arip18@gmail.com>
2022-01-14 01:21:22 +0800

    lalu comit

---
+++ app/Views/Pilihm.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="icon" href="gambar/plane.png">
+  <title>SWINGS.com</title>
+
+</head>
+
+<body>
+  <h1>Daftar Maskapai</h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col"> id </th>
+        <th scope="col"> nama </th>
+        <th scope="col"> gambar </th>
+        <th scope="col"> asal </th>
+        <th scope="col"> jam keberangkatan </th>
+        <th scope="col"> waktu </th>
+        <th scope="col"> jam tiba </th>
+        <th scope="col"> harga </th>
+        <th scope="col"> aksi </th>
+      </tr>
+    </thead>
+    <tbody>
+      <form action="">
+        <tr>
+          <th scope="row">1 </th>
+          <td>citilink</td>
+          <td>gambar dulu</td>
+          <td>cgk</td>
+          <td>04:30</td>
+          <td>1j 30m</td>
+          <td>06:00</td>
+          <td>833.400</td>
+          <td><button>pilih</button></td>
+        </tr>
+      </form>
+    </tbody>
+  </table>
+</body>
+
+</html>
