d5488d3602f293b13138fdd454757a798402374b
Alifan12 <alif129310@gmail.com>
2022-01-19 21:51:42 +0700

    commit

--- app/Views/Order.php
+++ app/Views/Order.php
@@ -2,120 +2,124 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="/css/OrderStyle.css">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="icon" href="/gambar/plane.png">
-    <title>SWINGS.com</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="/css/OrderStyle.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="icon" href="/gambar/plane.png">
+  <title>SWINGS.com</title>
 
 </head>
 
 <body>
-    <nav class="navbar">
-        <div class="navbar-left">
-            <div class="Judul">SWINGS.com</div>
-        </div>
-        <div class="navbar-right">
-            <ul>
-                <li>
-                    <img src="https://cdn-icons.flaticon.com/png/128/2102/premium/2102633.png?token=exp=1641823828~hmac=0ea66dd49c4f772b4e2993d861f32230"
-                        width="40px">
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <div class="grid-container">
-        <div class="item1">
-            <div class="detail">
-                <span>Jakarta</span>
-                <span>---></span>
-                <span>.</span>
-                <span>Tanggal</span>
-                <span>.</span>
-                <span>Penumpang</span>
-                <span>.</span>
-                <span>Kelas</span>
-            </div>
-        </div>
-        <div class="item2">
-            <button>Cari Penelusuran</button>
-        </div>
+  <nav class="navbar">
+    <div class="navbar-left">
+      <div class="Judul">SWINGS.com</div>
     </div>
+    <div class="navbar-right">
+      <ul>
+        <li>
+          <img
+            src="https://cdn-icons.flaticon.com/png/128/2102/premium/2102633.png?token=exp=1641823828~hmac=0ea66dd49c4f772b4e2993d861f32230"
+            width="40px">
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="grid-container">
+    <div class="item1">
+      <div class="detail">
+        <span>Jakarta</span>
+        <span>---></span>
+        <span>.</span>
+        <span>Tanggal</span>
+        <span>.</span>
+        <span>Penumpang</span>
+        <span>.</span>
+        <span>Kelas</span>
+      </div>
+    </div>
+    <div class="item2">
+      <button>Cari Penelusuran</button>
+    </div>
+  </div>
 
-    <!-- Body Pencarian Maskapai -->
-    <div class="container">
-        <div class="card-penerbangan">
-            <p class="kol6">Citilink Indonesia</p>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOy5NovtFWgOb3kowWhhUmRCdPGliqemHaifZhrSSGw8ml1rDS7nPf6-4EMjI4tjE5yc&usqp=CAU"
-                width="150px" outline="black" class="kol1">
-            <div class="kol2">
-                <p class="bold">04.30</p>
-                <p>CKG</p>
-            </div>
-            <div class="kol3">
-                <p style="border-bottom: 1pt solid" align="center">1j 30m</p>
-                <p align="center">Langsung</p>
-            </div>
-            <div class="kol4">
-                <p class="bold">06.00</p>
-                <p>SUB</p>
-            </div>
-            <div class="kol5">
-                <span class="bold-blue">IDR</span>
-                <span class="bold-blue">833.400</span>
-                <span style="color:gray">/pax</span>
-            </div>
-            <button class="kol7">Pilih</button>
-        </div>
-        <div class="card-penerbangan">
-            <p class="kol6">Citilink Indonesia</p>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOy5NovtFWgOb3kowWhhUmRCdPGliqemHaifZhrSSGw8ml1rDS7nPf6-4EMjI4tjE5yc&usqp=CAU"
-                width="150px" outline="black" class="kol1">
-            <div class="kol2">
-                <p class="bold">04.30</p>
-                <p>CKG</p>
-            </div>
-            <div class="kol3">
-                <p style="border-bottom: 1pt solid" align="center">1j 30m</p>
-                <p align="center">Langsung</p>
-            </div>
-            <div class="kol4">
-                <p class="bold">06.00</p>
-                <p>SUB</p>
-            </div>
-            <div class="kol5">
-                <span class="bold-blue">IDR</span>
-                <span class="bold-blue">833.400</span>
-                <span style="color:gray">/pax</span>
-            </div>
-            <button class="kol7">Pilih</button>
-        </div>
-        <div class="card-penerbangan">
-            <p class="kol6">Citilink Indonesia</p>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOy5NovtFWgOb3kowWhhUmRCdPGliqemHaifZhrSSGw8ml1rDS7nPf6-4EMjI4tjE5yc&usqp=CAU"
-                width="150px" outline="black" class="kol1">
-            <div class="kol2">
-                <p class="bold">04.30</p>
-                <p>CKG</p>
-            </div>
-            <div class="kol3">
-                <p style="border-bottom: 1pt solid" align="center">1j 30m</p>
-                <p align="center">Langsung</p>
-            </div>
-            <div class="kol4">
-                <p class="bold">06.00</p>
-                <p>SUB</p>
-            </div>
-            <div class="kol5">
-                <span class="bold-blue">IDR</span>
-                <span class="bold-blue">833.400</span>
-                <span style="color:gray">/pax</span>
-            </div>
-            <button class="kol7">Pilih</button>
-        </div>
+  <!-- Body Pencarian Maskapai -->
+  <div class="container">
+    <div class="card-penerbangan">
+      <p class="kol6">Citilink Indonesia</p>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOy5NovtFWgOb3kowWhhUmRCdPGliqemHaifZhrSSGw8ml1rDS7nPf6-4EMjI4tjE5yc&usqp=CAU"
+        width="150px" outline="black" class="kol1">
+      <div class="kol2">
+        <p class="bold">04.30</p>
+        <p>CKG</p>
+      </div>
+      <div class="kol3">
+        <p style="border-bottom: 1pt solid" align="center">1j 30m</p>
+        <p align="center">Langsung</p>
+      </div>
+      <div class="kol4">
+        <p class="bold">06.00</p>
+        <p>SUB</p>
+      </div>
+      <div class="kol5">
+        <span class="bold-blue">IDR</span>
+        <span class="bold-blue">833.400</span>
+        <span style="color:gray">/pax</span>
+      </div>
+      <button class="kol7">Pilih</button>
     </div>
+    <div class="card-penerbangan">
+      <p class="kol6">Citilink Indonesia</p>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOy5NovtFWgOb3kowWhhUmRCdPGliqemHaifZhrSSGw8ml1rDS7nPf6-4EMjI4tjE5yc&usqp=CAU"
+        width="150px" outline="black" class="kol1">
+      <div class="kol2">
+        <p class="bold">04.30</p>
+        <p>CKG</p>
+      </div>
+      <div class="kol3">
+        <p style="border-bottom: 1pt solid" align="center">1j 30m</p>
+        <p align="center">Langsung</p>
+      </div>
+      <div class="kol4">
+        <p class="bold">06.00</p>
+        <p>SUB</p>
+      </div>
+      <div class="kol5">
+        <span class="bold-blue">IDR</span>
+        <span class="bold-blue">833.400</span>
+        <span style="color:gray">/pax</span>
+      </div>
+      <button class="kol7">Pilih</button>
+    </div>
+    <div class="card-penerbangan">
+      <p class="kol6">Citilink Indonesia</p>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOy5NovtFWgOb3kowWhhUmRCdPGliqemHaifZhrSSGw8ml1rDS7nPf6-4EMjI4tjE5yc&usqp=CAU"
+        width="150px" outline="black" class="kol1">
+      <div class="kol2">
+        <p class="bold">04.30</p>
+        <p>CKG</p>
+      </div>
+      <div class="kol3">
+        <p style="border-bottom: 1pt solid" align="center">1j 30m</p>
+        <p align="center">Langsung</p>
+      </div>
+      <div class="kol4">
+        <p class="bold">06.00</p>
+        <p>SUB</p>
+      </div>
+      <div class="kol5">
+        <span class="bold-blue">IDR</span>
+        <span class="bold-blue">833.400</span>
+        <span style="color:gray">/pax</span>
+      </div>
+      <button class="kol7">Pilih</button>
+    </div>
+  </div>
 </body>
 
 </html>
