0e1558266441d25ee1fedf5f5ec2f6b1641c271d
L4lu4rdit44rip <ardita.arip18@gmail.com>
2022-01-20 02:25:50 +0800

    Lalu Ardita Arip coomit

---
+++ app/Views/refund.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="/css/Refund.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="/gambar/plane.png">
+    <title>SWINGS.com</title>
+
+</head>
+
+
+<body>
+    <nav class="navbar">
+        <div class="navbar-left">
+            <div class="Judul">SWINGS.com</div>
+        </div>
+        <div class="navbar-right">
+            <ul>
+                <li>
+                    <img src="https://cdn-icons.flaticon.com/png/128/2102/premium/2102633.png?token=exp=1641823828~hmac=0ea66dd49c4f772b4e2993d861f32230" width="40px">
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="atas1">
+            <div class="atas">
+                <p class="kol16">Kode Boking</p>
+            </div>
+            <div class="atas">
+                <p class="kol66">#########</p>
+            </div>
+        </div>
+        <div class="card-penerbangan">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOy5NovtFWgOb3kowWhhUmRCdPGliqemHaifZhrSSGw8ml1rDS7nPf6-4EMjI4tjE5yc&usqp=CAU" width="150px" outline="black" class="kol1">
+            <p class="kol6">Citilink Indonesia</p>
+            <div class="kol2">
+                <p>JAKARTA</p>
+            </div>
+            <div class="kol3">
+                <h2>></h2>
+            </div>
+            <div class="kol4">
+                <p>SURABAYA</p>
+            </div>
+            <div class="kol11">
+                <p>|</p>
+            </div>
+            <div class="kol12">
+                <p>Sen, 3 Januari 2021</p>
+            </div>
+            <div class="kol13">
+                <p>04.30</p>
+            </div>
+            <div class="re">
+                <button>Bisa Refund</button>
+            </div>
+        </div>
+        <button class="kol7">Lanjutkan</button>
+    </div>
+</body>
+
+</html>
