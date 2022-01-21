011935cff03f906163c3c7f90547dba28254273a
Alifan12 <alif129310@gmail.com>
2022-01-21 23:54:51 +0700

    alifa commit

--- app/Views/MetodePembayaran.php
+++ app/Views/MetodePembayaran.php
@@ -2,96 +2,96 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="/css/MetodeBayar.css">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="icon" href="/gambar/plane.png">
-    <title>SWINGS.com</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="/css/MetodeBayar.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="icon" href="/gambar/plane.png">
+  <title>SWINGS.com</title>
 
 </head>
 
 <body>
-    <nav class="navbar-menu">
-        <div class="navbar">
-            <div class="navbar-left">
-                SWINGS.com
-            </div>
+  <nav class="navbar-menu">
+    <div class="navbar">
+      <div class="navbar-left">
+        SWINGS.com
+      </div>
 
-            <div class="navbar-right">
-                <div class="logo">
-                </div>
-            </div>
+      <div class="navbar-right">
+        <div class="logo">
         </div>
-    </nav>
-    <div class="grid-container">
-        <div class="item1">
-            <div class="detail">
-                <span>Jakarta</span>
-                <span>---></span>
-                <span>.</span>
-                <span>Tanggal</span>
-                <span>.</span>
-                <span>Penumpang</span>
-                <span>.</span>
-                <span>Kelas</span>
-            </div>
-        </div>
-        <div class="item2">
-            <button>Cari Penelusuran</button>
-        </div>
+      </div>
     </div>
-    <div class="container">
-        <div class="kolom">
-            <div class="metode">
-                <h2>Metode pembayaran</h2>
-            </div>
-            <div class="id">
-                <div class="lokasi">
-                    Jakarta-->Surabaya
-                </div>
-                <div class="order_id">
-                    Order ID: 545226321
-                </div>
-
-            </div>
-            <div class="detail_harga">
-                <div class="harga">
-                    <h3>Detail harga </h3>
-                </div>
-                <div class="total">
-                    <p>Total pembayaran IDR 1.666.800</p>
-                </div>
-
-
-            </div>
-            <div class="virtual">
-                <div class="virtual_content">
-                    <div class="virtual_acc">
-                        <h1>Virtual Account</h1>
-                        <small>Anda bisa membayar dengan transfer melalui ATM, Internet Banking & Mobile Banking</small>
-                    </div>
-                    <div class="BCA">
-                        <span>BCA Virtual Account</span>
-                        <span><button class="choose"><img src="https://img.icons8.com/ios/50/000000/expand-arrow--v1.png" width="20px"></button></span>
-                    </div>
-                    <div class="BRI">
-                        <span>BRI Virtual Account</span>
-                        <span><button class="choose"><img src="https://img.icons8.com/ios/50/000000/expand-arrow--v1.png" width="20px"></button></span>
-
-                    </div>
-
-                </div>
-
-
-            </div>
-            <div class="submit">
-                <a href="EtiketController"><button type="submit">SUBMIT</button></a>
-            </div>
+  </nav>
+  <div class="grid-container">
+    <div class="item1">
+      <div class="detail">
+        <span>Jakarta</span>
+        <span>---></span>
+        <span>.</span>
+        <span>Tanggal</span>
+        <span>.</span>
+        <span>Penumpang</span>
+        <span>.</span>
+        <span>Kelas</span>
+      </div>
+    </div>
+    <div class="item2">
+      <button>Cari Penelusuran</button>
+    </div>
+  </div>
+  <div class="container">
+    <div class="kolom">
+      <div class="metode">
+        <h2>Metode pembayaran</h2>
+      </div>
+      <div class="id">
+        <div class="lokasi">
+          Jakarta-->Surabaya
+        </div>
+        <div class="order_id">
+          Order ID: 545226321
         </div>
 
+      </div>
+      <div class="detail_harga">
+        <div class="harga">
+          <h3>Detail harga </h3>
+        </div>
+        <div class="total">
+          <p>Total pembayaran IDR 1.666.800</p>
+        </div>
+
+
+      </div>
+      <div class="virtual">
+        <div class="virtual_content">
+          <div class="virtual_acc">
+            <h1>Virtual Account</h1>
+            <small>Anda bisa membayar dengan transfer melalui ATM, Internet Banking & Mobile Banking</small>
+          </div>
+          <div class="BCA">
+            <span>BCA Virtual Account</span>
+            <span><button class="choose"><img src="https://img.icons8.com/ios/50/000000/expand-arrow--v1.png"
+                  width="20px"></button></span>
+          </div>
+          <div class="BRI">
+            <span>BRI Virtual Account</span>
+            <span><button class="choose"><img src="https://img.icons8.com/ios/50/000000/expand-arrow--v1.png"
+                  width="20px"></button></span>
+
+          </div>
+
+        </div>
+      </div>
     </div>
+    <div class="submit">
+      <a href="EtiketController"><button type="submit">SUBMIT</button></a>
+    </div>
+  </div>
+
 </body>
 
 </html>
