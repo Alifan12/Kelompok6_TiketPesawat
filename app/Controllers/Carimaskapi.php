6107c6c5a13dbc60e261754d80e05d89bfd3b81e
akh.faris fz <akhfarisfz6017@gmail.com>
2022-01-21 21:41:07 +0700

    faris cmmit

--- app/Controllers/Carimaskapi.php
+++ app/Controllers/Carimaskapi.php
@@ -3,13 +3,18 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
-use App\Models\UserModel;
+use App\Models\maskapaiModel;
 
 
 class Carimaskapi extends Controller
 {
     public function index()
     {
+        // $db      = \Config\Database::connect();
+        // $builder = $db->table('penerbangan');
+        // $builder->select('*');
+        // $query = $builder->get();
+        // print_r($query->getResult());
         echo view('pages/caritiket');
     }
 }
