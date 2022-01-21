476abbe7bcbd8df83e15b6c59eaf7fc24d444471
akh.faris fz <akhfarisfz6017@gmail.com>
2022-01-21 13:27:46 +0700

    Faris Commit

--- app/Views/Profile.php
+++ app/Views/Profile.php
@@ -7,7 +7,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
-  <link rel="icon" href="gambar/plane.png">
+
+  <link rel="icon" href="/gambar/plane.png">
   <title>Profile</title>
 
 </head>
--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -7,7 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
-  <link rel="icon" href="gambar/plane.png">
+  <link rel="icon" href="/gambar/plane.png">
   <title>SWINGS.com</title>
 
 </head>
