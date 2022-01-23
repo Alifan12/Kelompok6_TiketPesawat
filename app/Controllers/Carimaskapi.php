5ebdc15ed7c9980becfb068cc135e6ac35db9fb9
Alifan12 <alif129310@gmail.com>
2022-01-23 23:25:03 +0700

    alifa commit

--- app/Controllers/Carimaskapi.php
+++ app/Controllers/Carimaskapi.php
@@ -20,14 +20,14 @@ class Carimaskapi extends Controller
     }
     public function cari()
     {
-        $this->request=\Config\Services::request();
+        $this->request = \Config\Services::request();
         $session = session();
-        $arrival=$this->request->getVar('arrival', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $depart=$this->request->getVar('departure', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $waktu_mulai=$this->request->getVar('tanggalberangkat', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $kelas=$this->request->getVar('kelas', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $arrival = $this->request->getVar('arrival', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $depart = $this->request->getVar('departure', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $waktu_mulai = $this->request->getVar('tanggalberangkat', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $kelas = $this->request->getVar('kelas', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $model = new penerbanganModel;
-        $data=$model-> cariPenerbangan($depart,$arrival,$waktu_mulai,$kelas);
-        return view('pages/order',$data);
+        $data = $model->cariPenerbangan($depart, $arrival, $waktu_mulai, $kelas);
+        return view('pages/order', $data);
     }
 }
