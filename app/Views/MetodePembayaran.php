bd84b13faf4cd889a5555ae0aef4f217f0d187b2
Alifan12 <alif129310@gmail.com>
2022-01-24 00:54:10 +0700

    alifa commit

--- app/Views/MetodePembayaran.php
+++ app/Views/MetodePembayaran.php
@@ -2,32 +2,32 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="/css/MetodeBayar.css">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="icon" href="/gambar/plane.png">
-    <title>SWINGS.com</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="/css/MetodeBayar.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="icon" href="/gambar/plane.png">
+  <title>SWINGS.com</title>
 
 </head>
 
 <body>
-    <nav class="navbar-menu">
-        <div class="navbar">
-            <div class="navbar-left">
-                SWINGS.com
-            </div>
+  <nav class="navbar-menu">
+    <div class="navbar">
+      <div class="navbar-left">
+        SWINGS.com
+      </div>
 
-            <div class="navbar-right">
-                <div class="logo">
-                </div>
-            </div>
+      <div class="navbar-right">
+        <div class="logo">
         </div>
-    </nav>
-    <div class="grid-container">
-       
-                <!-- <table>
+      </div>
+    </div>
+  </nav>
+  <div class="grid-container">
+
+    <!-- <table>
           <tr>
             <td>Jakarta</td>
             <td><img src="/gambar/DetailPemesan/right-arrow.png" width="10px"></td>
@@ -40,69 +40,69 @@
             <td>Kelass</td>
           </tr>
         </table> -->
-            </div>
+  </div>
+  </div>
+  <div class="item2">
+  </div>
+  </div>
+  <div class="container">
+    <div class="kolom">
+      <div class="metode">
+        <h2>Metode pembayaran</h2>
+      </div>
+      <div class="id">
+        <div class="lokasi">
+          <table>
+            <tr>
+              <td>Jakarta</td>
+              <td><img src="/gambar/DetailPemesan/right-arrow.png" width="10px"></td>
+              <td>Surabaya</td>
+            </tr>
+          </table>
         </div>
-        <div class="item2">
+        <div class="order_id">
+          <table>
+            <tr>
+              <td>Order ID : </td>
+              <td>1234567</td>
+            </tr>
+          </table>
         </div>
+
+      </div>
+      <div class="detail_harga">
+        <div class="harga">
+          <h3>Detail harga </h3>
+        </div>
+        <div class="total">
+          <p>Total pembayaran IDR 1.666.800</p>
+        </div>
+
+
+      </div>
+      <div class="virtual">
+        <div class="virtual_content">
+          <div class="virtual_acc">
+            <h1>Virtual Account</h1>
+            <small>Anda bisa membayar dengan transfer melalui ATM, Internet Banking & Mobile Banking</small>
+          </div>
+          <div class="BCA">
+            <span>BCA Virtual Account</span>
+            <span><a class="choose"><img src="/gambar/BRI.jpeg" width="50px"></a></span>
+          </div>
+          <div class="BRI">
+            <span>BRI Virtual Account</span>
+            <span><a class="choose"><img src="/gambar/BCA.jpeg" width="50px"></a></span>
+
+          </div>
+
+        </div>
+      </div>
     </div>
-    <div class="container">
-        <div class="kolom">
-            <div class="metode">
-                <h2>Metode pembayaran</h2>
-            </div>
-            <div class="id">
-                <div class="lokasi">
-                    <table>
-                        <tr>
-                            <td>Jakarta</td>
-                            <td><img src="/gambar/DetailPemesan/right-arrow.png" width="10px"></td>
-                            <td>Surabaya</td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="order_id">
-                    <table>
-                        <tr>
-                            <td>Order ID : </td>
-                            <td>1234567</td>
-                        </tr>
-                    </table>
-                </div>
-
-            </div>
-            <div class="detail_harga">
-                <div class="harga">
-                    <h3>Detail harga </h3>
-                </div>
-                <div class="total">
-                    <p>Total pembayaran IDR 1.666.800</p>
-                </div>
-
-
-            </div>
-            <div class="virtual">
-                <div class="virtual_content">
-                    <div class="virtual_acc">
-                        <h1>Virtual Account</h1>
-                        <small>Anda bisa membayar dengan transfer melalui ATM, Internet Banking & Mobile Banking</small>
-                    </div>
-                    <div class="BCA">
-                        <span>BCA Virtual Account</span>
-                        <span><a class="choose" ><img src="https://cdn-icons.flaticon.com/png/512/1633/premium/1633716.png?token=exp=1642855090~hmac=b862043756dce69aea44a0872e58ccaf" width="20px"></a></span>
-                    </div>
-                    <div class="BRI">
-                        <span>BRI Virtual Account</span>
-                        <span><a class="choose" ><img src="https://cdn-icons.flaticon.com/png/512/1633/premium/1633716.png?token=exp=1642855090~hmac=b862043756dce69aea44a0872e58ccaf" width="20px"></a></span>
-
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        <div class="submit">
-            <a href="EtiketController"><button>SUBMIT</button></a>
-        </div>
+    <div class="submit">
+      <a href="EtiketController"><button>SUBMIT</button></a>
     </div>
+  </div>
 </body>
 
 </html>
