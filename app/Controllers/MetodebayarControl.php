505f44a30bd32d25ac2dfc6ec0d0adbe144086f6
Alifan12 <alif129310@gmail.com>
2022-01-24 11:36:04 +0700

    alifa commit

--- app/Controllers/MetodebayarControl.php
+++ app/Controllers/MetodebayarControl.php
@@ -11,5 +11,17 @@ class MetodebayarControl extends BaseController
         $query   = $builder->get();  // Produces: SELECT * FROM mytable
         return view('MetodePembayaran.php');
     }
-    
+
+    // public function tampilPerjalanan()
+    // {
+    //     $this->request = \Config\Services::request();
+    //     $session = session();
+    //     $arrival = $this->request->getVar('arrival', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    //     $depart = $this->request->getVar('departure', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    //     $waktu_mulai = $this->request->getVar('tanggalberangkat', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    //     $kelas = $this->request->getVar('kelas', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    //     $model = new penerbanganModel;
+    //     $data = $model->cariPenerbangan($depart, $arrival, $waktu_mulai, $kelas);
+    //     return view('pages/order', $data);
+    // }
 }
